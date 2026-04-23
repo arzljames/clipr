5568f019980246b0945365f624b7b7b3dc02b2f7
Arzl <arzljames15@gmail.com>
2026-04-23 19:34:54 +0800

    integrate record creation

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,5 +1,6 @@
 import type { VerifyTokenResponse } from "@/types";
 import { axiosInstance } from "./axiosInstance";
+import { v4 as uuidv4 } from "uuid";
 
 export const verifyToken = async (
   token: string,
@@ -88,6 +89,7 @@ export const fileUpload = async (
   driver = import.meta.env.VITE_MEDIA_DRIVER,
   bucketName = import.meta.env.VITE_MEDIA_BUCKET_NAME,
 ) => {
+  const uuid = uuidv4();
   if (!binZUID || !driver || !bucketName) {
     throw new Error("Missing media upload environment variables.");
   }
@@ -107,6 +109,26 @@ export const fileUpload = async (
         },
       },
     );
+
+    if (response.status === 201 && response.data?.data?.length > 0) {
+      await axiosInstance.post(
+        `https://8-e8e981c5f6-2twrfl.api.zesty.io/v1/content/models/6-c29a99cfc8-c5gfdm/items`,
+        {
+          data: {
+            record: response.data.data[0].id,
+            comments: null,
+            record_id: uuid,
+          },
+          web: {
+            canonicalTagMode: 1,
+            parentZUID: "0",
+            pathPart: uuid,
+            metaTitle: uuid,
+          },
+          meta: { langID: 1, contentModelZUID: "6-c29a99cfc8-c5gfdm" },
+        },
+      );
+    }
 
     return response.data;
   } catch (error) {
